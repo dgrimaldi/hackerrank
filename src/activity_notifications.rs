d20e7e0c90c726728874a8186b655e603da1f0fd
dgrimaldi <grimi.david@gmail.com>
2023-08-17 13:38:27 +0200

    feat: implementation multithreads for activity notification

--- src/activity_notifications.rs
+++ src/activity_notifications.rs
@@ -1,46 +1,79 @@
 pub mod activity_notifications_mod {
-    pub trait Activity {
-        fn activity_notifications(&self) -> i32;
+    use std::sync::{Arc};
+    use std::sync::atomic::{AtomicI32, Ordering};
+    use std::thread;
+    use std::time::{Duration};
+
+    // Implementing a Trait on a Type
+    pub trait Activity  {
+        fn activity_notifications(&mut self) -> i32;
     }
-    pub struct Notification<'a>{
+    pub struct Notification{
         pub d: i32,
         pub n: i32,
-        pub expenditure: &'a[i32]
+        pub expenditure: Vec<i32>
     }
 
-    impl Notification<'_>  {
-        pub fn new(&self) -> &Notification {
-            self
-        }
+    #[derive(Debug)]
+    enum Median {
+        I32(i32),
+        F32(f32)
     }
 
-    impl Activity for Notification<'_>  {
-        fn activity_notifications(&self) -> i32 {
-            let mut number_of_notification = 0;
-            let is_even = (self.d % 2) == 0;
+
+    impl Activity for Notification {
+        fn activity_notifications(&mut self) -> i32 {
+            let d = self.d;
+            let n = self.n;
+            let expenditure = &mut self.expenditure;
+
+            let sort = |sliced_notification: &mut [i32]| sliced_notification.sort();
+
+            let number_of_notification = Arc::new(AtomicI32::new(0));
+
+            let is_even = (d % 2) == 0;
             let median_index = if is_even {
-                ((self.d / 2)as usize, (self.d / 2 ) as usize + 1)
+                ((d / 2) as usize, (d / 2) as usize + 1)
             } else {
-                (((self.d + 1) as usize / 2), 0usize)
+                (((d + 1) as usize / 2), 0usize)
             };
 
+            let start = d as usize;
+            let end = n as usize;
 
-            for i in 0..(self.n -self.d) {
-                let mut sliced_notification = Vec::from(&self.expenditure[i as usize..(self.d as usize + i as usize)]);
-                sliced_notification.sort();
 
-                let median: u32 = if is_even {
-                    ((sliced_notification[median_index.0 - 1] + sliced_notification[median_index.1 - 1]) / 2).try_into().unwrap()
-                } else {
-                    (sliced_notification[median_index.0 - 1]).try_into().unwrap()
-                };
+            for _ in 0..(end - start) {
 
-                if median*2 > self.expenditure[self.d as usize + i as usize] as u32 {
-                    number_of_notification += 1;
-                }
+                let mut exp = expenditure.clone();
 
+                let number_of_notification = number_of_notification.clone();
+
+                thread::spawn(move || {
+                    let sliced_notification = &mut exp[0..start];
+                    sort(sliced_notification);
+
+                    let median = if is_even {
+                        Median::F32((sliced_notification[median_index.0 - 1] + sliced_notification[median_index.1 - 1]) as f32 / 2.0)
+                    } else {
+                        Median::I32(sliced_notification[median_index.0 - 1])
+                    };
+
+                    match median {
+                        Median::I32(mediam_value) => if mediam_value * 2 <= exp[start] {  // let mut num = number_of_notification.lock().unwrap(); // *num += 1
+                            number_of_notification.fetch_add(1, Ordering::SeqCst);
+                        }
+                        Median::F32(mediam_value) => if mediam_value * 2.0 <= exp[start] as f32 {  // let mut num = number_of_notification.lock().unwrap(); // *num += 1;
+                            number_of_notification.fetch_add(1, Ordering::SeqCst);
+                        }
+                    }
+                });
+                thread::sleep(Duration::from_nanos(1));
+                expenditure.remove(0);
             }
-            number_of_notification
+
+
+
+            number_of_notification.load(Ordering::SeqCst)
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,27 @@
-use std::io;
-use std::os::unix::raw::nlink_t;
+use std::{env, fs, io};
+use std::time::SystemTime;
 use hackerrank_rust::activity_notifications::activity_notifications_mod::{Activity, Notification};
 use hackerrank_rust::mini_max_sum::mini_max_sum_mod::{mini_max_sum, Value};
 use hackerrank_rust::time_conversion::time_conversion_mod::{Meridian, time_conversion_impl};
 
 fn main() {
-
+    env::set_var("RUST_BACKTRACE", "1");
     let mut input = String::new();
     io::stdin()
         .read_line(&mut input)
         .expect("Failed to read line");
 
+
+    let sys_time = SystemTime::now();
+
     match input.trim() {
-        "1" => {println!("{:?}", mini_max_sum_fn())}
-        "2" => {println!("{:?}", time_conversion_fn())}
-        "3" => {println!("output {:?}", activity_notifications_fn())}
+        "1" => {print!("output {:?} ", mini_max_sum_fn())}
+        "2" => {print!("output{:?} ", time_conversion_fn())}
+        "3" => {print!("output {:?} ", activity_notifications_fn())}
         _ => {}
     }
+    println!("in {:?} ", SystemTime::now().duration_since(sys_time).unwrap());
+
     fn mini_max_sum_fn() -> (i128, i128) {
         let arr = Value::new([396285104,573261094,759641832,819230764,364801279]);
         mini_max_sum(arr)
@@ -28,7 +33,21 @@ fn main() {
 
 
     fn activity_notifications_fn() -> i32 {
-        let notification = Notification::new(&Notification {expenditure: &[2,3,4,2,3,6,8,4,5], d: 5, n: 9});
+        let args: Vec<String> = env::args().collect();
+
+        let file_path = &args[1];
+        let expenditure_a: Vec<i32> = fs::read_to_string(file_path)
+            .unwrap()
+            .split(' ')
+            .map(|s| s.to_string().parse::<i32>().unwrap())
+            .collect();
+
+        let expenditure_b: Vec<i32> = Vec::from([2,3,4,2,3,6,8,4,5]);
+
+        let mut notification = Notification {expenditure: expenditure_b, d: 5, n: 9};
+        notification.activity_notifications();
+
+        let mut notification = Notification {expenditure: expenditure_a, d: 10000, n: 200000};
         notification.activity_notifications()
     }
 
